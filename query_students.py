109a2c1401065beecbcd9c49a77bc3758955e0ec
Sasha007-star <bondarukoleksandra74@gmail.com>
2025-07-03 16:20:21 +0300

    Update query_students.py

--- query_students.py
+++ query_students.py
@@ -44,8 +44,56 @@ execute_query("""
     SELECT first_name, last_name, accessment, nationality
     FROM staff
     WHERE nationality LIKE 'Україн___'
-    ORDER BY accessment ASC
+    ORDER BY accessment DESC
     LIMIT 3
 """)
 
-print("\n Працівники у яких успішність ")
+print("\n Працівники у яких успішність така, що вони на межі вигнання з роботи")
+
+execute_query("""
+    SELECT first_name, last_name, accessment
+    FROM staff
+    WHERE accessment < '3.7'
+""")
+
+print("\n Працівники що народилися в 1900 роках і за гендером чоловік")
+
+execute_query("""
+    SELECT first_name, last_name, date_of_birth, gender
+    FROM staff
+    WHERE gender = 'чоловік' AND date_of_birth LIKE '19________'
+""")
+
+print("\n Хай програма перевірить який працівник що має хорошу успішність, в 154 чи 110 офісному кабінеті")
+
+execute_query("""
+    SELECT first_name, last_name, office_number, gender, accessment
+    FROM staff
+    WHERE office_number = 110 OR office_number = 154
+    ORDER BY accessment DESC
+""")
+
+print("\n Працівники що вступили на роботу в 2000-х роках і вони мають середню успішність")
+
+execute_query("""
+    SELECT first_name, last_name, employment_date, accessment
+    FROM staff, employment
+    WHERE employment_date LIKE '2010______'
+    ORDER BY accessment DESC
+""")
+
+print("\n Працівники які народилися між 2010-2021-х роках")
+
+execute_query("""
+    SELECT first_name, last_name, accessment
+    FROM staff
+    WHERE accessment BETWEEN 4 AND 4.9
+""")
+
+print("\n Показ яка у кого успішність ")
+
+execute_query("""
+    SELECT first_name, last_name, accessment
+    FROM staff
+    GROUP BY accessment
+""")
